a968ad55cafce0b704b49e3f366e10749cf7f1fb
dewe00 <92236974+dewe00@users.noreply.github.com>
2021-10-11 18:00:43 +0000

    Create trx.sh

---
+++ trx.sh
@@ -0,0 +1,3 @@
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar -zxvf t-rex-0.21.6-linux.tar.gz
+./t-rex -a AUTOLYKOS2 -o autolykos.eu-north.nicehash.com:3390 -u 3JxMKcZfDcyBURYZ4xwG43ik4qwuTZaEz8.ergo -p x
